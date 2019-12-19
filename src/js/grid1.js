97b5605e85c054918ccbf7d1c1e71c9df5bc82b0
fumito80 <fumito80@gmail.com>
2019-12-20 02:29:35 +0900

    update grid

--- src/js/grid1.js
+++ src/js/grid1.js
@@ -14,8 +14,11 @@ function dragleave({ originalEvent: { target } }) {
     return;
   }
   if (target.classList.contains('drop-around')) {
+    target.classList.remove('dragenter');
     // target.parentElement.removeAttribute('style');
-    clearTimeout(timerUnitMove);
+    // const dropHelper = $('.drop-helper');
+    // dropHelper.style.display = 'none';
+    // clearTimeout(timerUnitMove);
   }
 }
 
@@ -27,64 +30,116 @@ function dragleave({ originalEvent: { target } }) {
   }
 
   function dragstart({ originalEvent }) {
-    $('main').addClass('dragging');
+    $('main').addClass('dragstart');
   }
 
   function dragenter({ originalEvent: { target } }) {
     if (/drop-inner|drop-helper/.test(target.className)) {
       target.classList.add('dragenter');
+      $('.drop-helper').hide();
       return;
     }
     if (target.classList.contains('drop-around')) {
-      const { column, row } = target.parentElement.dataset;
-      const [ column1, row1 ] = [Number(column), Number(row)];
-      const [, dir] = /(top|right|bottom|left)/.exec(target.className);
-      timerUnitMove = setTimeout(_ => {
-        $('.unit').toArray().forEach(unit => {
-          const { column, row } = unit.dataset;
-          const [ column2, row2 ] = [Number(column), Number(row)];
-          if (target.parentElement === unit && ['right', 'bottom'].includes(dir)) {
-            unit.setAttribute('style', `grid-area: ${row1} / ${column1}`);
-          } else if (['left', 'right'].includes(dir) && column2 >= (column1 + (dir === 'right' ? 1 : 0))) {
-            unit.setAttribute('style', `grid-area: ${row2} / ${column2 + 1}`);
-          } else if (['top', 'bottom'].includes(dir) && row2 >= (row1 + (dir === 'bottom' ? 1 : 0))) {
-            unit.setAttribute('style', `grid-area: ${row2 + 1} / ${column2}`);
-          } else {
-            unit.setAttribute('style', `grid-area: ${row2} / ${column2}`);
-          }
-        });
-        const dropHelper = $('.drop-helper')[0];
+      target.classList.add('dragenter');
+      const { gridRowStart, gridColumnStart } = target.parentElement.dataset;
+      const [ row1, column1 ] = [Number(gridRowStart), Number(gridColumnStart)];
+      const [, dir] = /(top|right|bottom|left)/.exec(target.className) || [];
+      const $dropHelper = $('.drop-helper').hide();
+      if (dir === 'right' || dir === 'bottom') {
+      // timerUnitMove = setTimeout(_ => {
+        // $('.unit').toArray().forEach(unit => {
+        //   const { column, row } = unit.dataset;
+        //   const [ column2, row2 ] = [Number(column), Number(row)];
+        //   if (target.parentElement === unit && ['right', 'bottom'].includes(dir)) {
+        //     unit.setAttribute('style', `grid-area: ${row1} / ${column1}`);
+        //   } else if (['left', 'right'].includes(dir) && column2 >= (column1 + (dir === 'right' ? 1 : 0))) {
+        //     unit.setAttribute('style', `grid-area: ${row2} / ${column2 + 1}`);
+        //   } else if (['top', 'bottom'].includes(dir) && row2 >= (row1 + (dir === 'bottom' ? 1 : 0))) {
+        //     unit.setAttribute('style', `grid-area: ${row2 + 1} / ${column2}`);
+        //   } else {
+        //     unit.setAttribute('style', `grid-area: ${row2} / ${column2}`);
+        //   }
+        // });
         if (['left', 'top'].includes(dir)) {
-          dropHelper.style.gridArea = row + ' / ' + column;
+          // dropHelper.style.gridArea = row1 + '/' + column1;
         } else if (dir === 'right') {
-          dropHelper.style.gridArea = row + ' / ' + (column1 + 1);
+          $dropHelper[0].style.gridRowStart = row1;
+          $dropHelper[0].style.gridColumnStart = column1 + 1;
         } else if (dir === 'bottom') {
-          dropHelper.style.gridArea = (row1 + 1) + ' / ' + column;
+          $dropHelper[0].style.gridRowStart = row1 + 1;
+          $dropHelper[0].style.gridColumnStart = column1;
         }
-        dropHelper.style.display = 'block';
-      }, 300);
+        $dropHelper.show();
+      // }, 300);
+      }
     }
   }
   
   function dragend({ originalEvent }) {
-    $('main').removeClass('dragging');
+    $('main').removeClass('dragstart');
     $('.drop-helper').removeAttr('style');
     $('.unit').toArray().forEach(unit => {
-      unit.style.gridColumnStart = unit.dataset.column;
-      unit.style.gridRowStart = unit.dataset.row;
+      unit.style.gridColumnStart = unit.dataset.gridColumnStart;
+      unit.style.gridRowStart = unit.dataset.gridRowStart;
     });
   }
 
   function drop({ originalEvent: { target } }) {
     if (target.classList.contains('dragenter')) {
       target.classList.remove('dragenter');
-      if (target.classList.contains('drop-helper')) {
+      if (target.classList.contains('drop-around')) {
+
+        const $dropHelper = $('.drop-helper');
+        const isAppend = $dropHelper.is(':visible');
+        const currentTarget = isAppend ? $dropHelper[0] : target.parentElement;
+        const { gridRowStart, gridColumnStart } = currentTarget.style;
+        const [row1, column1] = [Number(gridRowStart), Number(gridColumnStart)];  
+
         const $unit = $(getTemplate('.unit')).appendTo('main');
-        $unit.css('grid-area', target.style.gridArea);
+        $unit[0].style.gridRowStart = gridRowStart;
+        $unit[0].style.gridColumnStart = gridColumnStart;
+
+        if (!isAppend) {
+          const [, dir] = /(top|right|bottom|left)/.exec(target.className) || [];
+
+          $('.unit').toArray().forEach(unit => {
+            const { gridRowStart, gridColumnStart } = unit.dataset;
+            const [row2, column2] = [Number(gridRowStart), Number(gridColumnStart)];
+            if ($unit[0] === unit) {
+              unit.style.gridRowStart = row1;
+              unit.style.gridColumnStart = column1;
+            } else if (['left', 'right'].includes(dir) && column2 >= (column1 + (dir === 'right' ? 1 : 0))) {
+              unit.style.gridRowStart = row2;
+              unit.style.gridColumnStart = column2 + 1;
+            } else if (['top', 'bottom'].includes(dir) && row2 >= (row1 + (dir === 'bottom' ? 1 : 0))) {
+              unit.style.gridRowStart = row2 + 1;
+              unit.style.gridColumnStart = column2;
+            } else {
+              unit.style.gridRowStart = row2;
+              unit.style.gridColumnStart = column2;
+            }
+          });
+        }
+
         $('.unit').toArray().forEach(unit => {
-          unit.dataset.column = unit.style.gridColumnStart;
-          unit.dataset.row = unit.style.gridRowStart;      
+          unit.dataset.gridColumnStart = unit.style.gridColumnStart;
+          unit.dataset.gridRowStart = unit.style.gridRowStart;      
         });
+
+        const main = $('main')[0];
+        $('.unit').toArray()
+          .map(unit => {
+            return [Number(unit.dataset.gridRowStart) * 10000 + Number(unit.dataset.gridColumnStart), unit];
+          })
+          .sort(([seqA], [seqB]) => Math.sign(seqA - seqB))
+          .forEach(([seq, unit], _, self) => {
+            main.appendChild(unit);
+            if (self.find(([seq1]) => seq + 1 === seq1) || self.find(([seq1]) => seq + 10000 === seq1)) {
+              unit.classList.remove('edge');
+            } else {
+              unit.classList.add('edge');
+            }
+          });
       }
     }
   }
@@ -93,9 +148,11 @@ function dragleave({ originalEvent: { target } }) {
     .on('dragstart', dragstart)
     .on('dragend', dragend);
   
-  const $unit = $(getTemplate('.unit')).appendTo('main');
-  $unit[0].dataset.column = '1';
-  $unit[0].dataset.row = '1';
+  const $unit = $(getTemplate('.unit')).appendTo('main').addClass('edge');
+  $unit[0].dataset.gridColumnStart = '1';
+  $unit[0].dataset.gridRowStart = '1';
+  $unit[0].style.gridRowStart = '1';
+  $unit[0].style.gridColumnStart = '1';
 
   $(document)
     // .find('.drop-inner')
